c79006b6f92cda90fb04e7cccd86d0cbbf8612be
tchounke <106194991+tchounke@users.noreply.github.com>
2023-05-14 16:40:13 -0400

    this commit add the identation lesson with python

---
+++ identation.py
@@ -0,0 +1,6 @@
+print("this is about identation")
+if 3 > 1:
+    print ("using if condition")
+    print ("we are comparing 3 & 1 for identation purpose")
+    
+print ("we are ok with the identation")
